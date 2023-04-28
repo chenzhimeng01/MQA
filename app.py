4b66963dd682f508bf4279d1dfbe92de1f39b3cf
ZhimengChen <58173996@qq.com>
2023-04-28 18:15:21 +0800

    optimize

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 
 app = Flask(__name__)
 
-model_checkpoint = "bert-finetuned-squad/checkpoint-33276"
+model_checkpoint = "roberta-finetuned-squad/checkpoint-33213"
 question_answerer = pipeline("question-answering", model=model_checkpoint)
 
 
@@ -18,25 +18,6 @@ def qa_text():
     res = question_answerer(question=question, context=context)
     res['context'] = context
     return res
-
-
-# @app.route('/qa-pdf', methods=['GET', 'POST'])
-# def qa_pdf():
-#     if request.method == 'POST':
-#         pdf_document = PyPDF2.PdfFileReader(request.files['file'])
-#
-#         question = request.form['question']
-#         print(question)
-#
-#         context = ''
-#         for i in range(pdf_document.numPages):
-#             page = pdf_document.getPage(i)
-#             context += page.extractText()
-#         print(context)
-#
-#         return question_answerer(question=question, context=context)
-#     else:
-#         return render_template('qa-pdf.html')
 
 
 if __name__ == '__main__':
